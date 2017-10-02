ea424d5d539906d5fe63c8d748d99c928223df4f
yadoch <yadoch@gmail.com>
2017-10-02 21:35:09 +0800

    完成DatePickerDialog 範例

--- app/src/main/java/tw/com/abc/mycalendarb35/MainActivity.java
+++ app/src/main/java/tw/com/abc/mycalendarb35/MainActivity.java
@@ -39,15 +39,25 @@ public class MainActivity extends AppCompatActivity {
                     now.get(Calendar.MONTH),
                     now.get(Calendar.DAY_OF_MONTH)
                     );
+            //DatePickerDialog 的內容設定
+            datePickerDlg.setTitle("選擇日期");
+            datePickerDlg.setMessage("請選擇適合您的日期");
+            datePickerDlg.setIcon(android.R.drawable.ic_dialog_info);
+            // 顯示Diaglog
+            datePickerDlg.show();
 
         }
     };
     private DatePickerDialog.OnDateSetListener aa =new DatePickerDialog.OnDateSetListener(){
-
-        @Override
+//
+//        @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-
+            mTxtResult.setText("您選擇的日期是" +
+                    Integer.toString(year) + "年" +
+                    Integer.toString(month+1) + "月" +
+                    Integer.toString(dayOfMonth) + "日");
         }
-    }
+        
+    };
 
 }
